f76a788eb2bc3961b4ce22ed925a4968fe0a44f3
Miyamoto-tryk <miyamoto_tryk@yahoo.co.jp>
2025-04-13 22:06:45 +0900

    article/pageに重い処理を追加

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,6 +1,8 @@
 import ArticleList from "@/components/article-list";
 
-export default function ArticlesPage() {
+export default async function ArticlesPage() {
+  //* 重いローディング処理　*/
+  await new Promise((res) => setTimeout(res, 1500));
   return (
     <main className="p-6">
       <h1 className="text-2xl font-bold mb-4">記事一覧</h1>
